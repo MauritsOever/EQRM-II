904428fa8c92757c66a859ad1d7d0ac8e2901ce4
MauritsOever <71381155+MauritsOever@users.noreply.github.com>
2021-11-24 09:46:38 +0100

    Some code changes and some plots

--- ass_1_main.py
+++ ass_1_main.py
@@ -7,7 +7,7 @@ Created on Tue Nov  9 12:01:46 2021
 
 @author: Maurits van den Oever and Connor Stevens
 """
-#%%
+
 # load in packages
 import pandas as pd
 import numpy as np
@@ -24,7 +24,7 @@ import statsmodels.tsa.stattools as st
 
 def loadin_data(path):
     data = pd.read_csv(path, sep = ";").iloc[:,1:]
-    # okay so the numbers have commas, so there interpreted as strings, lets see if we can change it
+    # okay so the numbers have commas, so they're interpreted as strings, lets see if we can change it
     for i in range(len(data.columns)):
         data.iloc[:,i] = data.iloc[:,i].apply(lambda x : x.replace(',', '.'))
         if i>0:    
@@ -58,7 +58,7 @@ def output_Q1(df):
     # for this question, we only use data until 2010
     # we dont have date indexing or whatever, so we dont know when to split the data
     # but we assume:
-    df = df.iloc[:5000,:] # this needs to change xdd
+    df = df[df['Day']<'01-01-2011']
     
     # magic numbers:
     cols = ['DJIA.Ret', 'N225.Ret', 'SSMI.Ret']
@@ -173,7 +173,7 @@ def output_Q1(df):
     
     print(summstats_df)
     print('')
-    # summstats_df.to_latex()
+    #print(summstats_df.to_latex())
     
     
     # Q1 f
@@ -195,10 +195,25 @@ def output_Q1(df):
             cov = np.sum((series_t-mean)*(series_tminus-mean))/(len(series_t))
             acfs[j, i-1] =  cov/var # assume stationarity?  
 
-        tstats[j,:] = acfs[j,:] / np.sqrt(1 + 2 * np.sum(acfs[j,:]**2)/len(acfs[j,:]))
+        tstats[j,:] = acfs[j,:] / (np.sqrt(1 + 2 * np.sum(acfs[j,:]**2))/(len(df)-1))
     pvals = sc.norm.pdf(tstats) # no significance, to be expected...
-    print('we should print some stuff here maybe for output...')
-    print('')
+    
+    q1f = pd.DataFrame()
+    q1f['DJIA acfs'] = acfs[0,:]
+    q1f['DJIA tstats'] = tstats[0,:]
+    q1f['DJIA pvals'] = pvals[0,:]
+    q1f['N225 acfs'] = acfs[1,:]
+    q1f['N225 tstats'] = tstats[1,:]
+    q1f['N225 pvals'] = pvals[1,:]
+    q1f['SSMI acfs'] = acfs[2,:]
+    q1f['SSMI tstats'] = tstats[2,:]
+    q1f['SSMI pvals'] = pvals[2,:]
+    q1f['index'] = range(1,13)
+    q1f = q1f.set_index('index')
+    q1f = q1f.round(decimals=4)
+    print(q1f.T)
+    #print(q1f.T.to_latex())
+    
     
     # Q1 g
     # get acfs for 100 lags...
@@ -215,7 +230,16 @@ def output_Q1(df):
             cov = np.sum((series_t-mean)*(series_tminus-mean))/(len(series_t))
             acfs[j, i-1] =  cov/var # assume stationarity?  
             
-    print('we should probs plot these...')
+    fig, ax = plt.subplots(nrows = 1,ncols = 3, figsize = (15, 4))
+    index = range(1,101)
+    ax[0].plot(index, acfs[0,:])
+    ax[0].set_title('DJIA')
+    ax[1].plot(index, acfs[1,:])
+    ax[1].set_title('N225')
+    ax[2].plot(index, acfs[2,:])
+    ax[2].set_title('SSMI')
+    plt.tight_layout()
+    plt.show()
     print('')
     
     return
@@ -255,7 +279,7 @@ def output_Q4(df):
     
             column_count += 1
     
-    fig, ax = plt.subplots(3,3, figsize= (10,10))
+    fig, ax = plt.subplots(3,3, figsize= (15,10))
     column_count = 0
     for i in range(0,3):
         for j in range(0,3):
@@ -312,14 +336,14 @@ def output_Q4(df):
     mu_hat_var1 = np.reshape(res_var1.x[0:3], (3,1))
     phi_hat_var1 = np.reshape(res_var1.x[3:], (3,3))
     # maybe print params...
-    print('mu = ', mu_hat_var1)
+    print('mu = ', mu_hat_var1.round(decimals=4))
     print('')
-    print('phi hat = ', phi_hat_var1)
+    print('phi hat = ', phi_hat_var1.round(decimals=4))
     print('')
     # get AIC, BIC and HIC
     eps_hat = y[:,1:] - phi_hat_var1@y[:,:-1] - mu_hat_var1
     sigma_hat_var1 = (eps_hat@eps_hat.T)/len(y[:,1:].T)
-    print('Sigma hat = ', sigma_hat_var1)
+    print('Sigma hat = ', sigma_hat_var1.round(decimals=4))
     print('')
     m = 0.5*3*(3+1) + 3 + 1*3**2
     T = len(y[:,1:].T)
@@ -329,7 +353,7 @@ def output_Q4(df):
     BIC_var1 = np.log(np.linalg.det(sigma_hat_var1)) + m*np.log(T)/T
     
     criterions_var1 = np.array([AIC_var1, AICc_var1, BIC_var1])
-    print('criterions of VAR(1) (AIC, AICc, BIC) are ', criterions_var1)
+    print('criterions of VAR(1) (AIC, AICc, BIC) are ', criterions_var1.round(decimals=4))
     print('')
     
     # get OLS estimates for VAR(2)
@@ -379,16 +403,16 @@ def output_Q4(df):
     phi1 = np.reshape(res_var2.x[3:12], (3,3))
     phi2 = np.reshape(res_var2.x[12:], (3,3))
     
-    print('mu hat = ', mu)
+    print('mu hat = ', mu.round(decimals=4))
     print('')
-    print('phi1 hat = ', phi1)
+    print('phi1 hat = ', phi1.round(decimals=4))
     print('')
-    print('phi1 hat = ', phi2)
+    print('phi2 hat = ', phi2.round(decimals=4))
     print('')
     
     eps_hat = y[:,2:] - phi1@y[:,1:-1] - phi2@y[:,:-2] - mu
     sigma_hat_var2 = (eps_hat@eps_hat.T)/len(y[:,2:].T)
-    print('sigma hat = ', sigma_hat_var2)
+    print('sigma hat = ', sigma_hat_var2.round(decimals=4))
     print('')
     m = 0.5*3*(3+1) + 3 + 2*3**2
     T = len(y[:,2:].T)
@@ -399,7 +423,7 @@ def output_Q4(df):
     
     criterions_var2 = np.array([AIC_var2, AICc_var2, BIC_var2])
     
-    print('criterions of VAR(2) (AIC, AICc, BIC) are ', criterions_var2)
+    print('criterions of VAR(2) (AIC, AICc, BIC) are ', criterions_var2.round(decimals=4))
     print('')
     
     #question 4 c
@@ -422,11 +446,12 @@ def output_Q4(df):
     P_hat_var2 = cholesky_decomp(sigma_hat_var2)
     
     print('P_hat for VAR(1) = ')
-    print(P_hat_var1)
+    print(P_hat_var1.round(decimals=4))
     print('')
     
     print('P_hat for VAR(2) = ')
-    print(cholesky_decomp(sigma_hat_var2))
+    print(cholesky_decomp(sigma_hat_var2).round(decimals=4))
+    print('')
     
     Q5_ests = {'P_hat_var1':P_hat_var1, 'P_hat_var2':P_hat_var2, 'phi_hat_var1':phi_hat_var1,
                'phi1': phi1, 'phi2':phi2}
@@ -434,7 +459,6 @@ def output_Q4(df):
     
     return Q5_ests
 
-#%%
 ###########################################################
 ### output_Q5
 def output_Q5(estimates):
@@ -451,6 +475,8 @@ def output_Q5(estimates):
     None.
 
     """
+    print('Question 5 IRF for VAR(1)')
+    print('')
     IRFlen = 11
     selection1 = np.reshape(np.array([1,0,0]), (3,1))
     selection2 = np.reshape(np.array([0,1,0]), (3,1))
@@ -465,7 +491,7 @@ def output_Q5(estimates):
         store2[i-1,:] = np.reshape(estimates['phi_hat_var1']**(i-1)@estimates['P_hat_var1']@selection2, (3,))
         store3[i-1,:] = np.reshape(estimates['phi_hat_var1']**(i-1)@estimates['P_hat_var1']@selection3, (3,))
         
-    fig, ax = plt.subplots(3,3, figsize= (10,10))
+    fig, ax = plt.subplots(3,3, figsize= (15,10))
     ax[0,0].plot(store1[:,0])
     ax[0,0].set_title('Effect of DJIA shock on DJIA')
     ax[0,1].plot(store1[:,1])
@@ -487,7 +513,8 @@ def output_Q5(estimates):
     plt.tight_layout()
     plt.show()
     
-    
+    print('Question 5 IRF for VAR(2)')
+    print('')
     ## okay now do IFRS for VAR(2)
     store1 = np.empty((IRFlen,3))
     store2 = np.empty((IRFlen,3))
@@ -504,7 +531,7 @@ def output_Q5(estimates):
             store3[i-1,:] = np.reshape(estimates['phi1']**(i-1)@estimates['P_hat_var2']@selection3 + estimates['phi2']**(i-1)@estimates['P_hat_var2']@selection1, (3,))
 
 
-    fig, ax = plt.subplots(3,3, figsize= (10,10))
+    fig, ax = plt.subplots(3,3, figsize= (15,10))
     ax[0,0].plot(store1[:,0])
     ax[0,0].set_title('Effect of DJIA shock on DJIA')
     ax[0,1].plot(store1[:,1])
@@ -526,14 +553,7 @@ def output_Q5(estimates):
     plt.tight_layout()
     plt.show()
 
-    
-    
     return
-
-#%%
-output_Q5(estimates)
-
-
 #%%
 ###########################################################
 ### main
@@ -542,7 +562,7 @@ def main():
     path = r"C:\Users\gebruiker\Documents\GitHub\EQRM-II\triv_ts.txt"
     df = loadin_data(path)
     
-    # output_Q1(df)
+    #output_Q1(df)
     
     estimates = output_Q4(df)
     output_Q5(estimates)
