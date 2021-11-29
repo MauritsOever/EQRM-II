c47093af4bedf8ebdff479c0d29199f42e04df4e
MauritsOever <71381155+MauritsOever@users.noreply.github.com>
2021-11-29 16:48:45 +0100

    Create ass_2_main.py

---
+++ ass_2_main.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+EQRM II
+Assignment 1 main
+
+Created on Mon Nov 29 16:00:04 2021
+
+@author: Donald Hagestein, Connor Stevens and Maurits van den Oever
+
+"""
+
+
+# import pachages
+import pandas as pd
+
+
+
+# define needed functions below
+# structure it so every question is its own function, that prints all of its output...
+# first the loadin_data(path) function
+
+
+###########################################################
+### loadin_data
+def loadin_data(path):
+    df = pd.read_csv(path)
+    df['date'] = pd.to_datetime(df['date'], format='%Y%m%d')
+    
+    # the test tickers
+    df_aapl = df[df['TICKER']=='AAPL'][['date', 'RET']].set_index('date').rename(columns={'RET':'AAPL_ret'})
+    df_msft = df[df['TICKER']=='MSFT'][['date', 'RET']].set_index('date').rename(columns={'RET':'MSFT_ret'})
+    df_csco = df[df['TICKER']=='CSCO'][['date', 'RET']].set_index('date').rename(columns={'RET':'CSCO_ret'})
+    
+    # the real tickers
+    df_mrk = df[df['TICKER']=='MRK'][['date', 'RET']].set_index('date').rename(columns={'RET':'MRK_ret'})
+    df_amzn = df[df['TICKER']=='AMZN'][['date', 'RET']].set_index('date').rename(columns={'RET':'AMZN_ret'})
+    df_pep = df[df['TICKER']=='PEP'][['date', 'RET']].set_index('date').rename(columns={'RET':'PEP_ret'})
+
+    df_test = pd.merge(pd.merge(df_aapl,df_msft,on='date'),df_csco,on='date')
+    df_real = pd.merge(pd.merge(df_mrk,df_amzn,on='date'),df_pep,on='date')
+
+    return df_test, df_real
+
+
+###########################################################
+### main
+def main():
+    # magic numbers
+    path = r"C:\Users\gebruiker\Documents\GitHub\EQRM-II\data_ass_2.csv"
+    df_test, df_real = loadin_data(path)
+    
+    
+    # now call the functions that print all of the output for all questions
+
+###########################################################
+### start main
+if __name__ == "__main__":
+    main()
