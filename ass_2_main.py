84a14a9520f21da7bdf7664af48972b5054faa25
MauritsOever <71381155+MauritsOever@users.noreply.github.com>
2021-12-01 13:37:28 +0100

    regularly scheduled update for question 4

--- ass_2_main.py
+++ ass_2_main.py
@@ -92,6 +92,82 @@ def output_Q1(df):
     
     return
 
+###########################################################
+### output_Q4
+def output_Q4(df, estimates):
+    """
+    Function that prints all the plots for question 4.
+
+    Parameters
+    ----------
+    df : dataframe of returns, used for getting estimated volatilities
+    estimates : output of parameter estimates from question 3
+
+    Returns
+    -------
+    None.
+
+    """
+    # placeholder estimates...
+    estimates = np.array([[0.0, 0.0, 0.9, 0.05, 0.4, 5, 1],
+                          [0.0, 0.0, 0.9, 0.05, 0.4, 5, 1],
+                          [0.0, 0.0, 0.9, 0.05, 0.4, 5, 1]])
+    
+    series = df.columns
+    # to do:
+        # get vola's for all series...
+        # get \sigma_2500
+        # get impact curve for \sigma_2500
+        # plot vola's w vertical line at t=2500
+        
+    def sigma_t(xlag1, mu, omega, beta, alpha, delta, Lambda, sigmalag1, lev):
+        # calculates the next sigma based on the xt and params
+        # every param is scalar, but lev is a bool
+        diffsq = (xlag1-mu)**2
+        if xlag1 < 0:
+            delta = 0
+        
+        if lev == True:
+            delta = 0
+        sigma_t = omega + (beta + ((alpha*diffsq + delta*diffsq)/(sigmalag1 + (1/Lambda)*diffsq)))*sigmalag1
+        
+        return sigma_t
+    
+    volas_lev = np.ones((len(df), 3)) # volatilities with leverage
+    volas_nolev = np.ones((len(df), 3)) # volatilities without leverage
+    fig, ax = plt.subplots(3,2)
+    
+    for i in range(1):
+        mu = estimates[i, 0]
+        omega = estimates[i, 1] 
+        beta = estimates[i, 2]
+        alpha = estimates[i, 3]
+        delta = estimates[i, 4]
+        Lambda = estimates[i, 5]
+        sigma1 = estimates[i, 6]
+        
+        for j in range(1, len(volas_lev)):
+            volas_lev[j,i] = sigma_t(df.iloc[j-1,i], mu, omega, beta, alpha, delta, Lambda, volas_lev[j-1,i], True)
+            volas_nolev[j,i] = sigma_t(df.iloc[j-1,i], mu, omega, beta, alpha, delta, Lambda, volas_nolev[j-1,i], False)
+        
+        # get range of xt...
+        # get answers of sigmat+1 based on this xt, for lev and no lev
+        # plot then
+        ax[i, 0].plot() # NIC here...
+        # title, axlabels, whatevs
+        ax[i, 1].plot(df.index, volas_lev[:,i]) # and maybe nolev as well??
+        
+        
+        
+    plt.tight_layout()
+    plt.show()
+        
+
+
+
+    return 
+
+
 
 
 #%% 
@@ -101,10 +177,11 @@ def main():
     # magic numbers
     path = r"C:\Users\gebruiker\Documents\GitHub\EQRM-II\data_ass_2.csv"
     df_test, df_real = loadin_data(path)
-    output_Q1(df_real)
     
     # now call the functions that print all of the output for all questions
-
+    output_Q1(df_real)
+    
+    
 ###########################################################
 ### start main
 if __name__ == "__main__":
