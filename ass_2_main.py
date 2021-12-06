cece44f8e71ac157b1060b94d924eff540e2839b
MauritsOever <71381155+MauritsOever@users.noreply.github.com>
2021-12-06 20:18:59 +0100

    insert Q3 and change Q4/5
    its runs with Q3 inserted but gives weird values. Still needs fixes before merging...

--- ass_2_main.py
+++ ass_2_main.py
@@ -16,6 +16,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as sc
 import datetime as dt
+from scipy.special import loggamma
+import scipy
+
 
 
 # define needed functions below
@@ -178,6 +181,368 @@ def output_Q2():
     
     return
 
+
+
+
+def output_Q3(data):
+    """
+    Function that handles all output for question 3
+
+    Parameters
+    ----------
+    data : Dataframe of returns
+
+    Returns
+    -------
+    None.
+
+    """
+    #### functions inside of functions ####
+    def printMatrix(s):
+        # Do heading
+        print("      ", end="")
+        for j in range(len(s[0])):
+            print("%7d " % j, end="")
+        print()
+        print("     ", end="")
+        for j in range(len(s[0])):
+            print("------", end="")
+        print()
+        # Matrix contents
+        for i in range(len(s)):
+            print("%3d |" % (i), end="")  # Row nums
+            for j in range(len(s[0])):
+                print("  ", round(s[i][j], 5), end="")
+            print()
+            
+    def variance(serie):
+        n=len(serie)
+        average=np.sum(serie)/n
+        serie=serie-average
+        serie=np.square(serie)
+        return np.sum(serie)/n
+    
+    def indicator(value):
+        placeholder=0
+        if value<0:
+            placeholder=1
+        return placeholder
+    
+    def t_logLikelihood(error,vega):
+        a=loggamma((vega+1)/2)
+        b=loggamma(vega/2)+0.5*np.log(vega*np.pi)
+        c=(-(vega+1)/2)*np.log(1+(error**2)/vega)
+        return (a-b)+c
+    
+    def _gh_stepsize(vP):
+        """
+        Purpose:
+            Calculate stepsize close (but not too close) to machine precision
+    
+        Inputs:
+            vP      1D array of parameters
+    
+        Return value:
+            vh      1D array of step sizes
+        """
+        vh = 1e-8*(np.fabs(vP)+1e-8)   # Find stepsize
+        vh= np.maximum(vh, 5e-6)       # Don't go too small
+    
+        return vh
+    ################################################################################
+    def jacobian_2sided(fun, vP, *args):
+        """
+        Purpose:
+        Compute numerical jacobian, using a 2-sided numerical difference
+    
+        Author:
+        Charles Bos, following Kevin Sheppard's hessian_2sided, with
+        ideas/constants from Jurgen Doornik's Num1Derivative
+    
+        Inputs:
+        fun     function, return 1D array of size iN
+        vP      1D array of size iP of optimal parameters
+        args    (optional) extra arguments
+    
+        Return value:
+        mG      iN x iP matrix with jacobian
+    
+        See also:
+        numdifftools.Jacobian(), for similar output
+        """
+        iP = np.size(vP)
+        vP= vP.reshape(iP)      # Ensure vP is 1D-array
+    
+        vF = fun(vP, *args)     # evaluate function, only to get size
+        iN= vF.size
+    
+        vh= _gh_stepsize(vP)
+        mh = np.diag(vh)        # Build a diagonal matrix out of h
+    
+        mGp = np.zeros((iN, iP))
+        mGm = np.zeros((iN, iP))
+    
+        for i in range(iP):     # Find f(x+h), f(x-h)
+            mGp[:,i] = fun(vP+mh[i], *args)
+            mGm[:,i] = fun(vP-mh[i], *args)
+    
+        vhr = (vP + vh) - vP    # Check for effective stepsize right
+        vhl = vP - (vP - vh)    # Check for effective stepsize left
+        mG= (mGp - mGm) / (vhr + vhl)  # Get central jacobian
+    
+        return mG
+    ################################################################################
+    def hessian_2sided(fun, vP, *args):
+        """
+        Purpose:
+        Compute numerical hessian, using a 2-sided numerical difference
+    
+        Author:
+        Kevin Sheppard, adapted by Charles Bos
+    
+        Source:
+        https://www.kevinsheppard.com/Python_for_Econometrics
+    
+        Inputs:
+        fun     function, as used for minimize()
+        vP      1D array of size iP of optimal parameters
+        args    (optional) extra arguments
+    
+        Return value:
+        mH      iP x iP matrix with symmetric hessian
+        """
+        iP = np.size(vP,0)
+        vP= vP.reshape(iP)    # Ensure vP is 1D-array
+    
+        f = fun(vP, *args)
+        vh= _gh_stepsize(vP)
+        vPh = vP + vh
+        vh = vPh - vP
+    
+        mh = np.diag(vh)            # Build a diagonal matrix out of vh
+    
+        fp = np.zeros(iP)
+        fm = np.zeros(iP)
+        for i in range(iP):
+            fp[i] = fun(vP+mh[i], *args)
+            fm[i] = fun(vP-mh[i], *args)
+    
+        fpp = np.zeros((iP,iP))
+        fmm = np.zeros((iP,iP))
+        for i in range(iP):
+            for j in range(i,iP):
+                fpp[i,j] = fun(vP + mh[i] + mh[j], *args)
+                fpp[j,i] = fpp[i,j]
+                fmm[i,j] = fun(vP - mh[i] - mh[j], *args)
+                fmm[j,i] = fmm[i,j]
+    
+        vh = vh.reshape((iP,1))
+        mhh = vh @ vh.T             # mhh= h h', outer product of h-vector
+    
+        mH = np.zeros((iP,iP))
+        for i in range(iP):
+            for j in range(i,iP):
+                mH[i,j] = (fpp[i,j] - fp[i] - fp[j] + f + f - fm[i] - fm[j] + fmm[i,j])/mhh[i,j]/2
+                mH[j,i] = mH[i,j]
+    
+        return mH
+    
+    def transformParameters_withLeverage(subparameters):
+        parameters=np.zeros(len(subparameters))
+        parameters[0] = subparameters[0]
+        parameters[1] = np.exp(subparameters[1])
+        parameters[2] = (1+np.exp(-subparameters[2]))**-1
+        parameters[3] = (1+np.exp(-subparameters[3]))**-1
+        parameters[4] = -1+2*(1+np.exp(-subparameters[4]))**-1
+        parameters[5] = np.exp(subparameters[5])
+        return parameters
+    
+    def transformParameters_withoutLeverage(subparameters):
+        parameters=np.zeros(len(subparameters))
+        parameters[0] = subparameters[0]
+        parameters[1] = np.exp(subparameters[1])
+        parameters[2] = (1+np.exp(-subparameters[2]))**-1
+        parameters[3] = (1+np.exp(-subparameters[3]))**-1
+        parameters[4] = np.exp(subparameters[4])
+        return parameters
+    
+    def RobustGARCHwithLeverageEffect(subparameters,serie,average):
+        parameters=transformParameters_withLeverage(subparameters)
+        x=serie
+        n=len(x)
+        mu = parameters[0]
+        omega = parameters[1]
+        alpha = parameters[2]
+        beta = parameters[3]
+        delta = parameters[4]
+        vega = parameters[5]
+        sigma_sq1=variance(x[0:50])
+        vSigma_sq=np.zeros(n)
+        vSigma_sq[0]=sigma_sq1
+        vLlik=np.zeros(n)
+        vLlik[0]=t_logLikelihood((x[0]-mu)/(vSigma_sq[0]**0.5),vega)-0.5*np.log(vSigma_sq[0])
+        i=1
+        while i<n:
+            vSigma_sq[i]=omega+(beta+(alpha*((x[i-1]-mu)**2)+delta*((x[i-1]-mu)**2)*indicator(x[i-1]))/(vSigma_sq[i-1]+(1/vega)*((x[i-1]-mu)**2)))*vSigma_sq[i-1]
+            vLlik[i]=t_logLikelihood((x[i]-mu)/(vSigma_sq[i]**0.5),vega)-0.5*np.log(vSigma_sq[i])
+            i=i+1
+    
+        if average==True:
+            return -np.mean(vLlik)
+        else:
+            return vLlik
+    
+    def RobustGARCHwithoutLeverageEffect(subparameters,serie,average):
+        parameters=transformParameters_withoutLeverage(subparameters)
+        x=serie
+        n=len(x)
+        mu = parameters[0]
+        omega = parameters[1]
+        alpha = parameters[2]
+        beta = parameters[3]
+        vega = parameters[4]
+        sigma_sq1=variance(x[0:50])
+        vSigma_sq=np.zeros(n)
+        vSigma_sq[0]=sigma_sq1
+        vLlik=np.zeros(n)
+        vLlik[0]=t_logLikelihood((x[0]-mu)/(vSigma_sq[0]**0.5),vega)-0.5*np.log(vSigma_sq[0])
+        i=1
+        while i<n:
+            vSigma_sq[i]=omega+(beta+(alpha*((x[i-1]-mu)**2))/(vSigma_sq[i-1]+(1/vega)*((x[i-1]-mu)**2)))*vSigma_sq[i-1]
+            vLlik[i]=t_logLikelihood((x[i]-mu)/(vSigma_sq[i]**0.5),vega)-0.5*np.log(vSigma_sq[i])
+            i=i+1
+    
+        if average==True:
+            return -np.mean(vLlik)
+        else:
+            return vLlik
+    
+    
+    def estimate_RobustGARCHwithLeverageEffect(serie):
+        n=len(serie)
+        x0 = [0]*6
+        average=True
+        res = scipy.optimize.minimize(RobustGARCHwithLeverageEffect,x0,args=(serie,average),method='SLSQP')
+        subparameters=res.x
+        parameters=transformParameters_withLeverage(subparameters)
+    
+        llik_sum=np.sum(RobustGARCHwithLeverageEffect(subparameters,serie,False))
+        llik_avg=np.mean(RobustGARCHwithLeverageEffect(subparameters,serie,False))
+        k=5
+        AIC=2*k-2*llik_sum
+        BIC=k*np.log(n)-2*llik_sum
+    
+        llikAVG = lambda subparameters: np.mean(RobustGARCHwithLeverageEffect(subparameters,serie=serie,average=False))
+        llikVector = lambda subparameters: RobustGARCHwithLeverageEffect(subparameters, serie=serie, average=False)
+        parameterTransformer = lambda subparameters: transformParameters_withLeverage(subparameters)
+        H = -hessian_2sided(llikAVG,vP=subparameters)
+        G = jacobian_2sided(llikVector, vP=subparameters)
+        G2 = (G.T @ G) / n
+        H_inv = np.linalg.inv(H)
+        Vhat = (H_inv @ G2 @ H_inv) / n
+        subparameterVariance=Vhat
+        K=jacobian_2sided(parameterTransformer, vP=subparameters)
+        parameterVariance=K @ subparameterVariance @K.T
+        parameters_STE=np.sqrt(np.diagonal(parameterVariance))
+    
+        # print(parameters)
+        # print(parameters_STE)
+    
+        output=np.zeros(15)
+        output[0]=parameters[0]
+        output[1]=parameters_STE[0]
+        output[2]=parameters[1]
+        output[3]=parameters_STE[1]
+        output[4]=parameters[2]
+        output[5]=parameters_STE[2]
+        output[6]=parameters[3]
+        output[7]=parameters_STE[3]
+        output[8]=parameters[4]
+        output[9]=parameters_STE[4]
+        output[10]=parameters[5]
+        output[11]=parameters_STE[5]
+        output[12] = llik_sum
+        output[13] = AIC
+        output[14] = BIC
+        return output
+    
+    def estimate_RobustGARCHwithoutLeverageEffect(serie):
+        n=len(serie)
+        x0 = [0]*5
+        average=True
+        res = scipy.optimize.minimize(RobustGARCHwithoutLeverageEffect,x0,args=(serie,average),method='SLSQP')
+        subparameters=res.x
+        parameters=transformParameters_withoutLeverage(subparameters)
+    
+        llik_sum=np.sum(RobustGARCHwithoutLeverageEffect(subparameters,serie,False))
+        llik_avg=np.mean(RobustGARCHwithoutLeverageEffect(subparameters,serie,False))
+        k=4
+        AIC=2*k-2*llik_sum
+        BIC=k*np.log(n)-2*llik_sum
+    
+        llikAVG = lambda subparameters: np.mean(RobustGARCHwithoutLeverageEffect(subparameters,serie=serie,average=False))
+        llikVector = lambda subparameters: RobustGARCHwithoutLeverageEffect(subparameters, serie=serie, average=False)
+        parameterTransformer = lambda subparameters: transformParameters_withoutLeverage(subparameters)
+        H = -hessian_2sided(llikAVG,vP=subparameters)
+        G = jacobian_2sided(llikVector, vP=subparameters)
+        G2 = (G.T @ G) / n
+        H_inv = np.linalg.inv(H)
+        Vhat = (H_inv @ G2 @ H_inv) / n
+        subparameterVariance=Vhat
+        K=jacobian_2sided(parameterTransformer, vP=subparameters)
+        parameterVariance=K @ subparameterVariance @K.T
+        parameters_STE=np.sqrt(np.diagonal(parameterVariance))
+    
+        # print(parameters)
+        # print(parameters_STE)
+    
+        output=np.zeros(15)
+        output[0]=parameters[0]
+        output[1]=parameters_STE[0]
+        output[2]=parameters[1]
+        output[3]=parameters_STE[1]
+        output[4]=parameters[2]
+        output[5]=parameters_STE[2]
+        output[6]=parameters[3]
+        output[7]=parameters_STE[3]
+        output[8]=None
+        output[9]=None
+        output[10]=parameters[4]
+        output[11]=parameters_STE[4]
+        output[12] = llik_sum
+        output[13] = AIC
+        output[14] = BIC
+        return output
+    
+    def question3_subTable(serie):
+        table=np.zeros((15,2))
+        table[:, 0] = estimate_RobustGARCHwithoutLeverageEffect(serie)
+        table[:, 1] = estimate_RobustGARCHwithLeverageEffect(serie)
+        return table
+    
+    #q3 body here
+    data=data[:2500].to_numpy()
+    data=data*100
+    print('len data: ',len(data))
+    stock1 = data[:, 0]
+    stock2 = data[:, 1]
+    stock3 = data[:, 2]
+
+    table=np.zeros((15,6))
+    table[:,0:2] = question3_subTable(stock1)
+    table[:,2:4] = question3_subTable(stock2)
+    table[:,4:6] = question3_subTable(stock3)
+
+    printMatrix(table)
+    df = pd.DataFrame(table)
+    writer = pd.ExcelWriter(r'C:\Users\gebruiker\Documents\GitHub\EQRM-II\iets.xlsx', engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Sheet1')
+    writer.save()
+    print('hey it actually worked')
+    
+    return table
+
+
 ###########################################################
 ### output_Q4
 def output_Q4(df, estimates):
@@ -210,6 +575,7 @@ def output_Q4(df, estimates):
         
         if lev == False:
             delta = 0
+            
         sigma_t = omega + (beta + ((alpha*diffsq + delta*diffsq)/(sigmalag1 + (1/Lambda)*diffsq)))*sigmalag1
         
         return sigma_t
@@ -219,19 +585,28 @@ def output_Q4(df, estimates):
     fig, ax = plt.subplots(3,2, figsize=(15,10))
     
     for i in range(3):
-        mu = estimates[i, 0]
-        omega = estimates[i, 1] 
-        beta = estimates[i, 2]
-        alpha = estimates[i, 3]
-        delta = estimates[i, 4]
-        Lambda = estimates[i, 5]
-        sigma1 = estimates[i, 6]
+        number = i*2
+        mu = estimates[0, number]
+        omega = estimates[2, number] 
+        beta = estimates[6, number]
+        alpha = estimates[4, number]
+        delta = estimates[8, number]
+        Lambda = estimates[10, number]
+        sigma1 = estimates[11, number]
         
-        volas_lev[0,i] = sigma1
+        mu_lev = estimates[0, number+1]
+        omega_lev = estimates[2, number+1]
+        beta_lev = estimates[6, number+1]
+        alpha_lev = estimates[4, number+1]
+        delta_lev = estimates[8, number+1]
+        Lambda_lev = estimates[10, number+1]
+        sigma1_lev = estimates[11, number+1]
+        
+        volas_lev[0,i] = sigma1_lev
         volas_nolev[0,i] = sigma1
         
         for j in range(1, len(volas_lev)):
-            volas_lev[j,i] = sigma_t(df.iloc[j-1,i], mu, omega, beta, alpha, delta, Lambda, volas_lev[j-1,i], True)
+            volas_lev[j,i] = sigma_t(df.iloc[j-1,i], mu_lev, omega_lev, beta_lev, alpha_lev, delta_lev, Lambda_lev, volas_lev[j-1,i], True)
             volas_nolev[j,i] = sigma_t(df.iloc[j-1,i], mu, omega, beta, alpha, delta, Lambda, volas_nolev[j-1,i], False)
         
         # get range of xt...
@@ -241,11 +616,8 @@ def output_Q4(df, estimates):
         NIC_lev = np.empty((len(rangext),))
         NIC_nolev = np.empty((len(rangext),))
         for t in range(len(rangext)):
-            NIC_lev[t] = sigma_t(rangext[t], mu, omega, beta, alpha, delta, Lambda, volas_lev[2500,i], True)
+            NIC_lev[t] = sigma_t(rangext[t], mu_lev, omega_lev, beta_lev, alpha_lev, delta_lev, Lambda_lev, volas_lev[2500,i], True)
             NIC_nolev[t] = sigma_t(rangext[t], mu, omega, beta, alpha, delta, Lambda, volas_lev[2500,i], False)     
-            #NIC_lev[t] = sigma_t(rangext[t], mu, omega, beta, alpha, delta, Lambda, 1, True)
-            #NIC_nolev[t] = sigma_t(rangext[t], mu, omega, beta, alpha, delta, Lambda, 1, False)            
-
             
         
         ax[i, 0].plot(rangext, NIC_lev) # NIC here...
@@ -270,7 +642,7 @@ def output_Q4(df, estimates):
     return 
 
 ###########################################################
-### output_Q4
+### output_Q5
 def output_Q5(df, estimates):
     """
     Function that prints all the plots for question 4.
@@ -301,6 +673,8 @@ def output_Q5(df, estimates):
         # calculates the next sigma based on the xt and params
         # every param is scalar, but lev is a bool
         diffsq = (xlag1-mu)**2
+        if diffsq < 0:
+            print('the error is here')
         
         if xlag1 > 0:
             delta = 0
@@ -319,22 +693,31 @@ def output_Q5(df, estimates):
     
     
     for i in range(3):
-        mu = estimates[i, 0]
-        omega = estimates[i, 1] 
-        beta = estimates[i, 2]
-        alpha = estimates[i, 3]
-        delta = estimates[i, 4]
-        Lambda = estimates[i, 5]
-        sigma1 = estimates[i, 6]
+        number = i*2
+        mu = estimates[0, number]
+        omega = estimates[2, number] 
+        beta = estimates[6, number]
+        alpha = estimates[4, number]
+        delta = estimates[8, number]
+        Lambda = estimates[10, number]
+        sigma1 = estimates[11, number]
+        
+        mu_lev = estimates[0, number+1]
+        omega_lev = estimates[2, number+1]
+        beta_lev = estimates[6, number+1]
+        alpha_lev = estimates[4, number+1]
+        delta_lev = estimates[8, number+1]
+        Lambda_lev = estimates[10, number+1]
+        sigma1_lev = estimates[11, number+1]
                 
         volas_lev = np.empty((len(df), 3)) # volatilities with leverage
         volas_nolev = np.empty((len(df), 3)) # volatilities without leverage
 
-        volas_lev[0,i] = sigma1
+        volas_lev[0,i] = sigma1_lev
         volas_nolev[0,i] = sigma1
         
         for j in range(1, len(volas_lev)):
-            volas_lev[j,i] = sigma_t(df.iloc[j-1,i], mu, omega, beta, alpha, delta, Lambda, volas_lev[j-1,i], True)
+            volas_lev[j,i] = sigma_t(df.iloc[j-1,i], mu_lev, omega_lev, beta_lev, alpha_lev, delta_lev, Lambda_lev, volas_lev[j-1,i], True)
             volas_nolev[j,i] = sigma_t(df.iloc[j-1,i], mu, omega, beta, alpha, delta, Lambda, volas_nolev[j-1,i], False)
         
         # get index for 2020 april 1 in df
@@ -352,7 +735,7 @@ def output_Q5(df, estimates):
         for n in range(simsize):
             eps = np.random.standard_t(Lambda, size=(21,))
             for j in range(1,21):
-                sims_sigma_lev[n,j] = sigma_t(sims_xt_lev[n,j-1], mu, omega, beta, alpha, delta, Lambda, sims_sigma_lev[n, j-1], True)
+                sims_sigma_lev[n,j] = sigma_t(sims_xt_lev[n,j-1], mu_lev, omega_lev, beta_lev, alpha_lev, delta_lev, Lambda_lev, sims_sigma_lev[n, j-1], True)
                 sims_sigma_nolev[n,j] = sigma_t(sims_xt_nolev[n,j-1], mu, omega, beta, alpha, delta, Lambda, sims_sigma_lev[n, j-1], False)
                 sims_xt_lev[n,j] = mu + sims_sigma_lev[n,j]*eps[j]
                 sims_xt_lev[n,j] = mu + sims_sigma_nolev[n,j]*eps[j]
@@ -398,12 +781,22 @@ def output_Q5(df, estimates):
 
     return
 
+#%%
+# magic numbers
+path = r"C:\Users\gebruiker\Documents\GitHub\EQRM-II\data_ass_2.csv"
+df_test, df_real = loadin_data(path)
+df = df_test
 
+estimates = output_Q3(df)
+output_Q4(df*100, estimates)
+output_Q5(df, estimates)
+
+#%%
 ###########################################################
 ### main
 def main():
     # magic numbers
-    path = r"data_ass_2.csv"
+    path = r"C:\Users\gebruiker\Documents\GitHub\EQRM-II\data_ass_2.csv"
     df_test, df_real = loadin_data(path)
 
     # now call the functions that print all of the output for all questions
